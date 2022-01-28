44a35898f27c6b235c311ea32174fc2cbdfae849
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-28 15:40:09 -0600

    added inputs and submit button and working console logs for onclick for submit button

--- client.js
+++ client.js
@@ -1,1 +1,21 @@
-console.log('helloWorld')
+$(document).ready(readyNow)
+
+
+function readyNow() {
+    $('.submitButton').on('click', submitButtonDo)
+
+
+    // lastInput
+    // idInput
+    // titleInput
+    // annualSalaryInput
+
+};
+
+function submitButtonDo (){
+    console.log('You added', $('#nameInput').val());
+    console.log('You added', $('#lastInput').val());
+    console.log('You added', $('#idInput').val());
+    console.log('You added', $('#titleInput').val());
+    console.log('You added', $('#annualSalaryInput').val());
+}
