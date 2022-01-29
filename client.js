fca92d27041ce23d65f6afc27076e70c589af518
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-29 15:55:21 -0600

    added deleteButton function

--- client.js
+++ client.js
@@ -12,13 +12,18 @@ function readyNow() {
 
 };
 
-function submitButtonDo (){
+function submitButtonDo() {
     console.log('You added', $('#nameInput').val());
     // tr <td>template literal tr
     $('#tableEmployees').append('<tr><td>' + $('#nameInput').val() + '</td><td>' + $('#lastInput').val() + '</td><td>' + + $('#idInput').val() + '</td><td>' + $('#titleInput').val() + '</td><td>' + $('#annualSalaryInput').val() + '</td></tr>');
     // $('#emp0').html($('#nameInput').val());
-    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
-}
+    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('') + //delete;
+};
+
+function deleteButton() {
+    $('#colDelete').empty('');
+
+};
 
 //appending, targeting parent, which is <table> or id tableEmployee
 
