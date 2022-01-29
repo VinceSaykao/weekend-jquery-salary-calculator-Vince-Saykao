c2a621a19ab5ca963baf94efcbb321fa1788310c
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-29 16:52:10 -0600

    added for loop

--- client.js
+++ client.js
@@ -1,38 +1,45 @@
 $(document).ready(readyNow)
 
 
-function readyNow() {
-    $('.submitButton').on('click', submitButtonDo)
-
-
-
-    // lastInput
-    // idInput
-    // titleInput
-    // annualSalaryInput
-
-};
-
 let employees = [];
 
 function addEmployee (fNameInput, lNameInput, iInput, tInput, sInput) {
     const newEmpObject = {
-        first: fNameInput,
+        first: $(),
         last: lNameInput,
-        id: iInput,
+        id: Number(iInput),
         title: tInput,
-        annualSalary: sInput,
+        annualSalary: Number(sInput),
     };
     addEmployee.push(newEmpObject);
     return true;
-}
+}; // end newEmployeeFunction
+
+function readyNow() {
+    $('.submitButton').on('click', random)
+}; // end readyNow function
+
+function random() {
+    let first = $('firstInput').val();
+    $('.submitButton').on('click', submitButtonDo); 
+    submitButtonDo();
+    
+
+};
+
+
 
 function submitButtonDo() {
     console.log('You added', $('#nameInput').val());
     // tr <td>template literal tr
-    $('#tableEmployees').append('<tr><td>' + $('#nameInput').val() + '</td><td>' + $('#lastInput').val() + '</td><td>' + + $('#idInput').val() + '</td><td>' + $('#titleInput').val() + '</td><td>' + $('#annualSalaryInput').val() + '</td></tr>' + '<td></td><button>${'Delete'}</button></td></td>');
+    // $('#theEmpList').empty();
+   
+    for (let emp of employees) {
+    $('#tableEmployees').append(`<tr><td>${emp.first}</td></tr>`);
+    // <td>${emp.last}</td><td>${emp.id}</td><td>${emp.title}</td><td>${emp.annualSalary}</td>
+    };
     // $('#emp0').html($('#nameInput').val());
-    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
+    // $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
     // deleteThis = $('td').append('<button class="addDelete">button</button>')
 };
 
