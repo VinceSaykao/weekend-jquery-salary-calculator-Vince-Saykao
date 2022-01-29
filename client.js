5c70121b0674cea7419893a20cc3363973b74b6d
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-29 16:13:23 -0600

    added addEmployee function and objects

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ function readyNow() {
     $('.submitButton').on('click', submitButtonDo)
 
 
+
     // lastInput
     // idInput
     // titleInput
@@ -12,12 +13,27 @@ function readyNow() {
 
 };
 
+let employees = [];
+
+function addEmployee (fNameInput, lNameInput, iInput, tInput, sInput) {
+    const newEmpObject = {
+        first: fNameInput,
+        last: lNameInput,
+        id: iInput,
+        title: tInput,
+        annualSalary: sInput,
+    };
+    addEmployee.push(newEmpObject);
+    return true;
+}
+
 function submitButtonDo() {
     console.log('You added', $('#nameInput').val());
     // tr <td>template literal tr
-    $('#tableEmployees').append('<tr><td>' + $('#nameInput').val() + '</td><td>' + $('#lastInput').val() + '</td><td>' + + $('#idInput').val() + '</td><td>' + $('#titleInput').val() + '</td><td>' + $('#annualSalaryInput').val() + '</td></tr>');
+    $('#tableEmployees').append('<tr><td>' + $('#nameInput').val() + '</td><td>' + $('#lastInput').val() + '</td><td>' + + $('#idInput').val() + '</td><td>' + $('#titleInput').val() + '</td><td>' + $('#annualSalaryInput').val() + '</td></tr>' + '<td></td><button>${'Delete'}</button></td></td>');
     // $('#emp0').html($('#nameInput').val());
-    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('') + //delete;
+    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
+    // deleteThis = $('td').append('<button class="addDelete">button</button>')
 };
 
 function deleteButton() {
