8c4e9b72c9c4ae345df304c9bb55230252fe08b2
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-30 20:42:40 -0600

    updated displayEmployees function, added button

--- client.js
+++ client.js
@@ -1,38 +1,59 @@
 $(document).ready(readyNow)
 
-let employees = [];
+class newEmployeeFunction {
+    constructor(firstName, lastName, employeeId, title, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeId = employeeId;
+        this.title = title;
+        this.annualSalary = annualSalary;
 
-function employeeList (fNameInput, lNameInput, iInput, tInput, sInput) {
-    const newEmpObject = {
-        first: fNameInput,
-        last: lNameInput,
-        id: Number(iInput),
-        title: tInput,
-        annualSalary: Number(sInput),
-    };
-    employees.push(newEmpObject);
-    calculateMonthlyCost();
-    
-}; // end newEmployeeFunction
+    }
+};  // end newEmployeeFunction
+
+// global scope for employees array
+let employees = [];
 
 // readyNow function
 function readyNow() {
-    $('.submitButton').on('click', submitButtonDo)
-    $('#inputEmp').on('click', 'tr', deleteButton)
+    $('.submitButton').on('click', submitButtonDo);
+    $('#inputEmp').on('click', 'tr', deleteButton);
+    $('#deleteButton').on('click', handleDelete);
 }; // end readyNow function
+
+function deleteButton() {
+    console.log('deleted');
+    $(this).remove();
+
+}; // end of deleteButton function
+
+    function handleDelete() {
+const idToDelete = $("#IdDelete").val();
+    for (let i = 0; i < employees.length; i++) {
+        if (idToDelete === employees[i].employeeId)
+            employees.splice(i, 1);
+    }
+    displayEmployees();
+    }; // end of handleDelete function
 
 
 // function for actions for what submit button will do when clicked
 function submitButtonDo() {
     console.log('You added', $('#nameInput').val(),$('#lastInput').val(),$('#idInput').val(),$('#titleInput').val(),$('#annualSalaryInput').val());
-    // let first = $('#nameInput').val();
-    // let last = $('#lastInput').val();
-    // let id = $('#idInput').val();
-    // let title = $('#titleInput').val();
-    // let annualSalary = $('#annualSalaryInput').val();
- 
-    // employeeList(first,last,id,title,annualSalary);
-   
+    let newEmployee = new newEmployeeFunction($('#nameInput').val(), $('#lastInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
+    employees.push(newEmployee);
+    displayEmployees();
+    calculateMonthlyCost();
+    
+    
+  
+    
+    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
+    
+}; // end of submitButtonDo function
+
+function displayEmployees() {
+    // displays input values
     $('#inputEmp').append(
         '<tr><td>' 
     + $('#nameInput').val() 
@@ -44,19 +65,22 @@ function submitButtonDo() {
     + '</td><td>' 
     + `<button class="deleteButton">DELETE</button>` 
         + '</td></tr>');
+    // end of input values
 
-    //empty the inputs-boxes
-    $('#nameInput').val(''),$('#lastInput').val(''),$('#idInput').val(''),$('#titleInput').val(''),$('#annualSalaryInput').val('');
-    
-}; // end of submitButtonDo function
+    console.log("in displayEmployees");
+    let el = $('#inputEmp');
+    el.empty();
 
-function deleteButton() {
-    console.log('deleted')
-    $(this).remove();
+    for (let employee of employees) {
 
-    calculateMonthlyCost();
+        let outputString = '<tr class="table-dark">';
+        outputString += '<td>' + employee.firstName + '</td>' + '<td>' + employee.lastName + '</td>' + '<td>' + employee.employeeId + '</td>' + '<td>' + employee.title + '</td>' + '<td class="yearCash" >' + employee.annualSalary + '</td>' + '<td>' + `<button class="deleteButton">DELETE</button>` + '</td>' + '</tr>';
+        el.append(outputString);
 
-}; // end of deleteButton function
+}; // end of for loop
+}; // end of displayEmployees function
+
+
 
 
 
